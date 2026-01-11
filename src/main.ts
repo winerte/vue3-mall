d16a5f63a0ef4015e783b799c28dcd7a76eee318
te <winerte@163.com>
2026-01-11 16:53:53 +0800

    引入router ，注意 官方示例createMemoryHistory的坑要替换createWebHistory，配置路径别名

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import 'virtual:windi.css'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createMemoryHistory, createWebHistory,createRouter } from 'vue-router'
+import Index from '～/pages/index.vue'
+import About from '～/pages/about.vue'
+import NotFound from '～/pages/404.vue'
+
+
+
+const routes = [
+  { path: '/', component: Index },
+  {
+    path: '/about',
+
+    component: About
+  },
+  { path: '/:pathMatch(.*)*', component: NotFound },
+]
+
+const router = createRouter({
+  history: createWebHistory(), //createMemoryHistory()
+  routes,
+})
+
+export default router
