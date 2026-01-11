f49c7acecc13af1dfc43ce3adbbd4b5c71211668
te <winerte@163.com>
2026-01-11 17:12:39 +0800

    404页面

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,3 +1,7 @@
 <template>
-    <h1>404</h1>
+    <el-result icon="warning" title="404页面错误" subTitle="点击下方按钮返回首页">
+      <template #extra>
+        <el-button type="primary" size="medium" @click="$router.push('/')">返回首页</el-button>
+      </template>
+    </el-result>
 </template>
